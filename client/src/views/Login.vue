216102abcd3b82fcecc33c8da6323504c625fa34
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-11 13:08:42 -0600

    add a login page, not yet linked to the backend

---
+++ client/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .login
+    h1 Login
+    form.login-form()
+      .username-field
+        label Username
+        input(type="text" name="username")
+      .password-field
+        label Password
+        input(type="password" name="password")
+      .submit-button
+        input(type="submit")
+</template>
