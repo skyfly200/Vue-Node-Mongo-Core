e612de69c1819fba68f8a1238596d336e71330d9
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-11 13:09:48 -0600

    remove redundant comment

--- client/src/router.ts
+++ client/src/router.ts
@@ -25,9 +25,6 @@ export default new Router({
     {
       path: "/login",
       name: "login",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "login" */ "./views/Login.vue")
     }
