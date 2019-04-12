b8a5808072437b065fc09e77d116da9f2354da9f
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-12 01:09:46 -0600

    resetup routing and improve login page

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,23 +16,20 @@
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
+  name: "App",
+  data() {
+    return {};
   }
-}
+};
 </script>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,13 +1,34 @@
 <template lang="pug">
   .login
     h1 Login
-    form.login-form()
-      .username-field
-        label Username
-        input(type="text" name="username")
-      .password-field
-        label Password
-        input(type="password" name="password")
-      .submit-button
-        input(type="submit")
+    v-form.login-form(@onSubmit="submit" v-model="valid")
+      v-text-field( v-model="username"
+        :rules="usernameRules"
+        label="Username"
+        required)
+      v-text-field( v-model="password"
+        :rules="passwordRules"
+        label="Password"
+        required)
+      v-btn(@click="submit") Submit
 </template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  data: () => ({
+    valid: false,
+    username: "",
+    usernameRules: [v => !!v || "Username is required"],
+    password: "",
+    passwordRules: [v => !!v || "Password is required"]
+  }),
+  methods: {
+    submit: () => {
+      console.log("Submit");
+    }
+  }
+})
+export default class Home extends Vue {}
+</script>
