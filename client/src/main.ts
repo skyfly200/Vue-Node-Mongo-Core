77946fde4ec77cdefb1276f4ece0b2ab5c77079e
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-12 01:35:31 -0600

    add a common axios config for dev server

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import { HTTP } from "./http-common";
 
 Vue.config.productionTip = false;
 
