01d686927f7b151eeb7f6ebc8ab02d4d87fde189
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-13 11:33:05 -0600

    refactor login response handler & remove all refrences to response.user

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -35,27 +35,28 @@ import { Component, Vue } from "vue-property-decorator";
     login: function(e) {
       if (this.username && this.password)
         this.$http
-          .post("http://localhost:1234/user/login", { username, password })
+          .post("http://localhost:1234/users/login", {
+            username: this.username,
+            password: this.password
+          })
           .then(response => {
-            let is_admin = response.data.user.is_admin;
-            localStorage.setItem("user", JSON.stringify(response.data.user));
-            localStorage.setItem("jwt", response.data.token);
-
-            if (localStorage.getItem("jwt") != null) {
-              this.$emit("loggedIn");
-              if (this.$route.params.nextUrl != null) {
-                this.$router.push(this.$route.params.nextUrl);
-              } else {
-                if (is_admin === 1) {
-                  this.$router.push("admin");
+            if (response.data && response.data.auth) {
+              // MUST be changed to store JWT in cookie for security!!!
+              localStorage.setItem("jwt", response.data.token);
+              if (localStorage.getItem("jwt") != null) {
+                this.$emit("loggedIn");
+                if (this.$route.params.nextUrl != null) {
+                  this.$router.push(this.$route.params.nextUrl);
                 } else {
                   this.$router.push("dashboard");
                 }
               }
+            } else {
+              console.log("Auth failed");
             }
           })
           .catch(function(error) {
-            console.error(error.response);
+            console.error(error);
           });
     }
   }
