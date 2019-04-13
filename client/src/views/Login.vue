0c91d841c48cbe60bc5bce349c9252739e055e6b
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-13 10:36:14 -0600

    store jwt token in local storage

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -36,7 +36,24 @@ import { Component, Vue } from "vue-property-decorator";
       if (this.username && this.password)
         this.$http
           .post("http://localhost:1234/user/login", { username, password })
-          .then(response => {})
+          .then(response => {
+            let is_admin = response.data.user.is_admin;
+            localStorage.setItem("user", JSON.stringify(response.data.user));
+            localStorage.setItem("jwt", response.data.token);
+
+            if (localStorage.getItem("jwt") != null) {
+              this.$emit("loggedIn");
+              if (this.$route.params.nextUrl != null) {
+                this.$router.push(this.$route.params.nextUrl);
+              } else {
+                if (is_admin === 1) {
+                  this.$router.push("admin");
+                } else {
+                  this.$router.push("dashboard");
+                }
+              }
+            }
+          })
           .catch(function(error) {
             console.error(error.response);
           });
