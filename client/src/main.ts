b124aabaff49abfbe4c3503f16ba4471a1e5b394
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-13 15:01:33 -0600

    set default auth header for axios, with jwt token

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,6 +8,11 @@ import Axios from "axios";
 
 Vue.prototype.$http = Axios;
 
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
+
 Vue.config.productionTip = false;
 
 new Vue({
