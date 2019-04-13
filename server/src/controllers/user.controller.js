37edc60af66e00558d71e4782c15f557851edd75
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-13 09:46:58 -0600

    standardize login responses

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -15,17 +15,17 @@ exports.login = async (req, res, next) => {
         const error = new Error('An Error occured')
         return next(error);
       } else if (!user) {
-        return res.json({ err: 'Invalid Credentials' });
+        return res.json({ auth: false, err: 'Invalid Credentials' });
       }
       req.login(user, { session : false }, async (error) => {
         if( error ) return next(error)
         //We don't want to store the sensitive information such as the
-        //user password in the token so we pick only the username and id
-        const body = { _id : user._id, username : user.username };
+        //user password in the token so we pick only the username, roles and id
+        const body = { _id : user._id, username : user.username, roles: user.roles };
         //Sign the JWT token and populate the payload with the user email and id
         const token = jwt.sign({ user : body },'top_secret');
         //Send back the token to the user
-        return res.json({ token });
+        return res.json({ auth: true, token: token });
       });
     } catch (error) {
       return next(error);
