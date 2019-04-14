f5c2fc81298d5361aaa03391629488bc9a442265
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-14 17:33:30 -0600

    add profileand groups pages

---
+++ client/src/views/Groups.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="groups">
+    <h1>Find Grouos to Join</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Groups"
+};
+</script>
+
+<style lang="sass" scoped></style>
---
+++ client/src/views/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="profile">
+    <h1>{{ username }}'s Profile</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+  computed: {
+    username: function() {
+      return this.$store.getters.user.username;
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped></style>
