7257a00407f6d0ad4a826e7ad60180346ed0a339
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-15 05:33:20 -0600

    improve 404 styles and template

--- client/src/views/404.vue
+++ client/src/views/404.vue
@@ -4,18 +4,34 @@ v-container(fluid grid-list-md).error-404
     v-flex.heading
       h1 404
       h2 Page not found
-    v-flex.links
-      router-link(to="/") Back to Home
+    v-flex.body
+      .home-link
+        v-btn(to="/") Back to Home
       h4 Email webmaster about broken links
+      a(:href="'mailto:' + email")
+        h4 {{ email }}
 </template>
 
 <script>
-export default {
-  name: "404"
-};
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  data: () => ({
+    email: "webmaster@example.com"
+  })
+})
+export default class Error404 extends Vue {}
 </script>
 
 <style lang="sass" scoped>
+.heading
+  margin: 10%
+.body
+  display: flex
+  justify-content: center
+  .v-btn
+    width: 200px
+    margin: 50px 0
 .layout
   display: flex
   flex-direction: column
