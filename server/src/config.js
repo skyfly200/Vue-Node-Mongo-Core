925bf1c22acc2b272f06d127e0c44e3dfe3a5746
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-16 23:41:02 -0600

    remove config.js from tracking

--- server/src/config.js
+++
@@ -1,8 +0,0 @@
-exports.tokenSecret = "catsarethebest";
-exports.sessionSecret = "awesomecookiesecret";
-exports.expressPort = 1234;
-exports.dbInfo = {
-  host: 'localhost',
-  port: '27017',
-  db: 'CRNH'
-};
