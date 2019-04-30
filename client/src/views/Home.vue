da7624d8306dec5d31824e6e4f6fe7acaf51a5ad
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-30 03:49:06 -0600

    remove vuetify sample home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,13 @@
 <template lang="pug">
 v-container(fluid='')
   .home
-    HelloWorld(msg="Welcome to Your Vue.js + TypeScript App")
+    h1 Welcome
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
+@Component({})
 export default class Home extends Vue {}
 </script>
 
