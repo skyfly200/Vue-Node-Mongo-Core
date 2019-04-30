260ea353af088da6ea8b0b8211a45bc74bb869a2
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-29 22:09:25 -0600

    ImgUpload: add drag n drop, refactor and redesign

--- client/src/components/ImgUpload.vue
+++ client/src/components/ImgUpload.vue
@@ -4,13 +4,20 @@ v-dialog(v-model="toggle").image-upload-dialog
     v-card-title(class="headline") Upload a new {{ type }} image
     v-card-text
       .uploader(v-if="noFiles")
-        .drop_zone Drag and drop {{ multi ? "images" : "image"}} here
-        h3 or
-        input(type="file" label="Browse images" :multiple="multi" @change="load" append-icon="photo_camera")
-        h4 {{ sizeLimit }}MB max per image
+        input.file-input(type="file" accept="image/*" ref="fileInput" :multiple="multi" @change="select")
+        form.drop-zone(v-if="dragAndDropCapable" ref="dropZone" :class="{ dragging: dragging }")
+          v-icon image
+          p Drag and drop here
+          span or
+          br
+          v-btn.browse-btn(@click="$refs.fileInput.click()" color="primary") Choose a file
+          h4 {{ sizeLimit }}MB size limit
+        .browse-only(v-else)
+          v-btn.browse-btn(@click="$refs.fileInput.click()" color="primary") Choose a file
+          h4 {{ sizeLimit }}MB size limit
         v-alert(:value="error" type="error") {{ message }}
       .image-preview(v-else)
-        v-img(v-for="f in files" :src="f" width="200px")
+        v-img(v-for="f in fileUrls" :src="f" width="200px")
         v-btn(@click="clear") Clear
         v-alert(:value="error" type="error") {{ message }}
     v-card-actions
@@ -24,53 +31,99 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({
   props: ["type", "toggle", "multi"],
   data: () => ({
+    reader: new FileReader(),
     files: [],
+    fileUrls: [],
+    dragAndDropCapable: false,
+    dragging: false,
     types: ["image.*"],
     message: "",
     error: false,
     sizeLimit: 10 // in MB
   }),
+  mounted(){
+    this.dragAndDropCapable = this.determineDragAndDropCapable();
+  },
+  updated(){
+    // wait until the entire view has been rendered
+    this.$nextTick(() => {
+      if( this.dragAndDropCapable && this.$refs.dropZone ){
+        // bind an event listener to all of the drag events
+        ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach( function( evt ) {
+          this.$refs.dropZone.addEventListener(evt, function(e){
+            e.preventDefault();
+            e.stopPropagation();
+          }.bind(this), false);
+        }.bind(this));
+        ['dragover', 'dragenter'].forEach( function( evt ) {
+          this.$refs.dropZone.addEventListener(evt, function(e) {this.dragging = true}.bind(this));
+        }.bind(this), false);
+        ['dragleave', 'dragend', 'drop'].forEach( function( evt ) {
+          this.$refs.dropZone.addEventListener(evt, function(e) {this.dragging = false}.bind(this));
+        }.bind(this), false);
+        this.$refs.dropZone.addEventListener('drop', function(e){
+          for( let i = 0; i < e.dataTransfer.files.length; i++ ){
+            this.files.push( e.dataTransfer.files[i] );
+            console.log(e.dataTransfer.files[i], i);
+            this.load(e.dataTransfer.files[i]);
+          }
+        }.bind(this));
+      }
+    });
+  },
   computed: {
     noFiles: function() {
       return this.files.length < 1;
     }
   },
   methods: {
-    load: function(e) {
+    select: function(e) {
       // || TODO || show spinning loader here to improve UX
       var fileList = e.target.files;
-      var reader = new FileReader();
       this.clear();
-      for (var f in fileList) {
-        let file = fileList[f];
-        // Check file against type and size constraints
-        if (file.type && !this.checkType(file)) {
-          this.message = "Invalid File Type";
-          this.error = true;
-        } else if (file.size && !this.checkSize(file)) {
-          this.message = "Oversized File";
-          this.error = true;
-        } else if (file.type && file.size) {
-          // Setup file reader callback
-          reader.onload = ( (cb) => {
-            return (e) => { cb(e.target.result) };
-          })(result => this.files.push(result));
-          // Call reader with file
-          reader.readAsDataURL(file);
-        }
+      for (var i=0; i<fileList.length; i++) {
+        this.load(fileList[i]);
       }
     },
+    load: function(file) {
+      // Check file against type and size constraints
+      if (!this.checkType(file)) {
+        this.message = "Invalid File Type";
+        this.error = true;
+      } else if (!this.checkSize(file)) {
+        this.message = "Oversized File";
+        this.error = true;
+      } else {
+        this.files.push(file);
+        this.show(file);
+      }
+    },
+    show: function(file) {
+      var reader = new FileReader();
+      // Register file reader events
+      reader.onload = (cb => { return e => {cb(e.target.result)} })(result => {
+        this.fileUrls.push(result)
+      } );
+      reader.onerror = (cb => { return e => {cb(e.target.error)} })(error => {this.message = error; this.error = true;} );
+      // Call reader with file
+      reader.readAsDataURL(file);
+    },
+    determineDragAndDropCapable(){
+      var div = document.createElement('div');
+      return ( ( 'draggable' in div ) || ( 'ondragstart' in div && 'ondrop' in div ) )
+              && 'FormData' in window && 'FileReader' in window;
+    },
     checkType: function(file) {
-      let validType;
-      for (let t in this.types) validType = validType || file.type && file.type.match(this.types[t]);
-      return validType;
+      if (!file.type) return false;
+      for (let t in this.types) if (file.type.match(this.types[t])) return true;
+      return false;
     },
     checkSize: function(file) {
-      let byteSize = this.sizeLimit * 1048576;
-      return file.size <= byteSize;
+      return !!file.size || file.size <= (this.sizeLimit * 1048576);
     },
     clear: function() {
       this.files = [];
+      this.fileUrls = [];
       this.message = "";
       this.error = false;
     },
@@ -100,14 +153,20 @@ export default class ImgUpload extends Vue {}
   h3
     text-align: center
     margin: 10px
-  input
-    margin: 25px auto
-.drop_zone
+.drop-zone
   border: 2px dashed #bbb
   border-radius: 5px
   padding: 25px
   text-align: center
   color: #bbb
+.dragging
+  background-color: grey
+.browse-only
+  text-align: center
+.file-input
+  display: none
+.browse-btn
+  margin: 1.5em auto
 .v-alert
   width: 100%
 </style>
