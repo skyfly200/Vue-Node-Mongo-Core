60c8ba0979e77fba81b70bf1cd671823fadca2c8
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-01 00:41:34 -0600

    add a drawer menu

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,44 +1,65 @@
 <template lang="pug">
-v-toolbar(app scroll-off-screen)
-  v-toolbar-side-icon
-  v-toolbar-title.headline.text-uppercase
-    span PWA Core
-    span.font-weight-light Vue, Express, Mongo
-  v-spacer
-  v-toolbar-items#nav.hidden-sm-and-down
-    v-btn(flat small to='/') Home
-    v-btn(flat small to='/about') About
-    template(v-if='isLoggedIn')
-      v-btn(flat small to='/groups') Groups
-      v-btn(flat small to='/chat') Chat
-      v-btn(flat small to='/dashboard') Dashboard
-  v-divider(vertical)
-  v-toolbar-items#user
-    template(v-if='isLoggedIn')
-      .user-img
-        v-img(src="http://lorempixel.com/200/200/nature" width="40")
-      .controls
-        .welcome
-          span Welcome&nbsp;
-          router-link(to='/profile') {{ username }}
-        .logout
-          v-btn(flat small @click='logout') Logout
-    template(v-else)
-      .login
-        v-btn(flat small to='/login') Login
-      .register
-        v-btn(flat small to='/register') Register
+#nav
+  v-navigation-drawer(app v-model="drawer")
+    v-list.pa1
+      v-list-tile(avatar)
+        v-list-tile-avatar
+          v-img(src="https://randomuser.me/api/portraits/men/85.jpg")
+        v-list-tile-content
+          v-list-tile-title {{ username }}
+            .logout
+              v-btn(flat small @click='logout') Logout
+      v-divider
+      v-list-tile
+        v-list-tile-content
+          v-list-tile-title title
+  v-toolbar(app scroll-off-screen)
+    v-toolbar-side-icon(@click="drawer = !drawer")
+    v-toolbar-title.headline.text-uppercase
+      span PWA Core
+      span.font-weight-light Vue, Express, Mongo
+    v-spacer
+    v-toolbar-items#links.hidden-sm-and-down
+      v-btn(flat small to='/') Home
+      v-btn(flat small to='/about') About
+      template(v-if='isLoggedIn')
+        v-btn(flat small to='/groups') Groups
+        v-btn(flat small to='/chat') Chat
+        v-btn(flat small to='/dashboard') Dashboard
+    v-divider(vertical)
+    v-toolbar-items#user
+      template(v-if='isLoggedIn')
+        .user-img
+          v-img(src="http://lorempixel.com/200/200/nature" width="40")
+        .controls
+          .welcome
+            span Welcome&nbsp;
+            router-link(to='/profile') {{ username }}
+          .logout
+            v-btn(flat small @click='logout') Logout
+      template(v-else)
+        .login
+          v-btn(flat small to='/login') Login
+        .register
+          v-btn(flat small to='/register') Register
 </template>
 <script>
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({
+  data: () => ({
+    drawer: null
+  }),
   computed: {
     isLoggedIn: function() {
       return this.$store.getters.isLoggedIn;
     },
     username: function() {
       return this.$store.getters.user.username;
+    },
+    img: function() {
+      // Need to implement this getter and backend data flow
+      //return this.$store.getters.user.img;
     }
   },
   methods: {
@@ -52,7 +73,7 @@ import { Component, Vue } from "vue-property-decorator";
 export default class Nav extends Vue {}
 </script>
 <style lang="sass" scoped>
-#nav
+#links
   margin: auto 1em
 #user
   margin-left: 24px
