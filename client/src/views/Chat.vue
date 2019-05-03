72fc56f080acbaa1583ec4793d6b9e8ec966184b
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-03 11:37:11 -0600

    fix convo avatar selector function

--- client/src/views/Chat.vue
+++ client/src/views/Chat.vue
@@ -15,7 +15,8 @@ v-container(fluid grid-list-md).chat
           v-divider(v-if="i > 0")
           v-list-tile.conversation(@click="selectConvo(i)")
             v-list-tile-avatar
-              v-img(:src="selectConvoAvatar(c)")
+              v-img(v-if="c.members.length > 1" :src="selectConvoAvatar(c)")
+              v-icon(v-else large) person
             v-list-tile-content(:class="{ unread: c.unread }")
               v-list-tile-title
                 h5 {{ c.title }}
@@ -29,11 +30,26 @@ v-container(fluid grid-list-md).chat
     v-flex.active-conversation(sm8)
       v-toolbar.view-toolbar(flat dense)
         template(v-if="editRecipients || !isRecipients")
-          v-autocomplete.recipients(label="To" chips full-width hide-details hide-no-data hide-selected multiple single-line
-            v-model="recipients"
+          v-autocomplete.recipients(label="To" chips full-width return-object hide-details hide-no-data hide-selected multiple single-line dense
+            v-model="conversations[selected].members"
             :items="contacts"
+            item-text="username"
+            item-value="username"
             append-outer-icon="check"
             @click:append-outer="editRecipients = false")
+            template(v-slot:selection='data')
+              v-chip.chip--select-multi(:selected='data.selected' close @input='removeRecipient(data.item)')
+                v-avatar
+                  img(:src='data.item.avatar')
+                | {{ data.item.username }}
+            template(v-slot:item='data')
+              template(v-if="typeof data.item !== 'object'")
+                v-list-tile-content(v-text='data.item')
+              template(v-else)
+                v-list-tile-avatar
+                  img(:src='data.item.avatar')
+                v-list-tile-content
+                  v-list-tile-title {{ data.item.username }}
         template(v-else-if="editTitle")
           v-text-field.title-edit(name="title" label="Conversation Title" single-line full-width hide-details
             v-model="conversations[selected].title"
@@ -43,23 +59,23 @@ v-container(fluid grid-list-md).chat
           v-spacer
           v-toolbar-title.title-view {{ conversations[selected].title }}
           v-spacer
-          v-btn(v-if="!isMulti" small icon @click="editTitle = true")
+          v-btn(v-if="isMulti" small icon @click="editTitle = true")
             v-icon(small) edit
-          v-btn(v-if="!isMulti" small icon @click="editRecipients = true")
+          v-btn(v-if="isMulti" small icon @click="editRecipients = true")
             v-icon(small) person_add
       .body
         .messages
           v-list
             v-list-tile.message(v-for="m in conversations[selected].messages")
               v-list-tile-avatar(v-if="m.author !== username")
-                v-img(:src="getAvitar(m.author)")
+                v-img(:src="getAvatar(m.author)")
               v-list-tile-content
                 v-list-tile-sub-title
                   .author(v-if="isMulti && m.author !== username") {{ m.author }}
                   .message-body {{ m.body }}
                   .timestamp Sent: {{ m.timestamp }}
               v-list-tile-avatar(v-if="m.author === username")
-                v-img(:src="getAvitar(m.author)")
+                v-img(:src="getAvatar(m.author)")
         v-form.reply-bar(@submit.prevent="sendMessage")
           v-text-field(name="reply" v-model="reply" label="Reply" :disabled="!isRecipients" autofocus single-line full-width hide-details).reply-field
           v-btn(fab small color="green" @click="sendMessage").send
@@ -80,11 +96,10 @@ import { Component, Vue } from "vue-property-decorator";
       editRecipients: false,
       editTitle: false,
       contacts: [
-        {username: this.$store.getters.user.username, avitar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"},
-        {username: "test2", avitar: "https://cdn.vuetifyjs.com/images/lists/2.jpg"},
-        {username: "test3", avitar: "https://cdn.vuetifyjs.com/images/lists/3.jpg"},
-        {username: "test4", avitar: "https://cdn.vuetifyjs.com/images/lists/4.jpg"},
-        {username: "test5", avitar: "https://cdn.vuetifyjs.com/images/lists/5.jpg"},
+        {username: "test2", avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg"},
+        {username: "test3", avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg"},
+        {username: "test4", avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg"},
+        {username: "test5", avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg"},
       ],
       conversations: [
         {
@@ -93,8 +108,8 @@ import { Component, Vue } from "vue-property-decorator";
           title: "Example",
           creator: this.$store.getters.user.username,
           members: [
-            {username: this.$store.getters.user.username, avitar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"},
-            {username: "test2", avitar: "https://cdn.vuetifyjs.com/images/lists/2.jpg"}
+            {username: this.$store.getters.user.username, avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"},
+            {username: "test2", avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg"}
           ],
           messages: [
             { author: "test2", body: "This is a message from another user", timestamp: "8 mins ago" },
@@ -109,8 +124,8 @@ import { Component, Vue } from "vue-property-decorator";
           title: "Example 2",
           creator: "test3",
           members: [
-            {username: this.$store.getters.user.username, avitar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"},
-            {username: "test3", avitar: "https://cdn.vuetifyjs.com/images/lists/3.jpg"}
+            {username: this.$store.getters.user.username, avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"},
+            {username: "test3", avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg"}
           ],
           messages: [
             { author: "test3", body: "Hey, whats up?", timestamp: "2 hours ago" }
@@ -156,7 +171,7 @@ import { Component, Vue } from "vue-property-decorator";
           title: "New Conversation",
           creator: this.username,
           members: [
-            {username: this.username, avitar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"}
+            {username: this.username, avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"}
           ],
           messages: []
         };
@@ -169,20 +184,26 @@ import { Component, Vue } from "vue-property-decorator";
       this.editRecipients = false;
       this.editTitle = false;
     },
+    loadRecipients: function() {},
     deleteConvo: function(i) {
       this.conversations.splice(i,1);
+    },
+    removeRecipient: function(user) {
+      const index = this.conversations[this.selected].members.indexOf(user.username);
+      if (index >= 0) this.conversations[this.selected].members.splice(index, 1);
     },
     getLast: function(c) {
       return c.messages.length - 1;
     },
-    getAvitar: function(author) {
+    getAvatar: function(author) {
       let member = this.conversations[this.selected].members.find( (m) => (m.username === author) );
-      return member.avitar;
+      return member.avatar;
     },
     selectConvoAvatar: function(c) {
-      let username = c.creator;
-      let avitar = c.members.find( (m) => (m.username === username) ).avitar;
-      return avitar;
+      let members = c.members.filter( (m) => (m.username !== this.username));
+      console.log(members)
+      let avatar = members.length ? members[0].avatar : null;
+      return avatar;
     },
     titleCase: function(string) {
       if (string) return string.charAt(0).toUpperCase() + string.slice(1);
