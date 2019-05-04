cff478a1f12faa39b9736fd29cba67192dcb31a6
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-04 14:59:47 -0600

    Imlement Message component

--- client/src/components/chat/Message.vue
+++ client/src/components/chat/Message.vue
@@ -1,10 +1,20 @@
 <template lang="pug">
+  v-list-tile.message
+    v-list-tile-avatar(v-if="!own")
+      v-img(:src="avatar")
+    v-list-tile-content
+      v-list-tile-sub-title
+        .author(v-if="showName") {{ author }}
+        .message-body {{ body }}
+        .timestamp {{ timestamp }}
+    v-list-tile-avatar(v-if="own")
+      v-img(:src="avatar")
 </template>
 <script>
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({
-  props: []
+  props: ["author", "avatar", "body". "timestamp", "own", "showName"]
 })
 export default class Message extends Vue {}
 </script>
