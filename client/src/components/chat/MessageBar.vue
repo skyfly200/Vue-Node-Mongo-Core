4b253717ac317b08ba72812fc386bdfcc2014d88
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-06 22:10:45 -0600

    modify chat options templates, standardize new & old options selection

--- client/src/components/chat/MessageBar.vue
+++ client/src/components/chat/MessageBar.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   v-toolbar.view-toolbar(flat dense)
-    template(v-if="menu === 'search'")
+    template(v-if="menu === 'recipients' || !isRecipients")
+      UserSelector(:previous="conversation.members" :contacts="contacts" @done="updateRecipients($event)")
+    template(v-else-if="menu === 'search'")
       v-text-field.search(name="search" label="Search Conversation" single-line full-width hide-details
         v-model="query"
         prepend-icon="search"
@@ -11,18 +13,16 @@
     template(v-else-if="menu === 'mute'")
       v-select.mute(name="mute" label="Mute Notifications Period" single-line full-width hide-details
         v-model="mute"
-        :items="['1 hour','6 hours','12 hours','24 hours','1 week','Till I turn them back on']"
+        :items="['Keep on','1 hour','6 hours','12 hours','24 hours','1 week','Till I turn them back on']"
         prepend-icon="notifications_off"
         append-outer-icon="check"
         @click:append-outer="menu = ''")
-    template(v-else-if="editTitle")
+    template(v-else-if="menu === 'title'")
       v-text-field.title-edit(name="title" label="Conversation Title" single-line full-width hide-details
         v-model="title"
         prepend-icon="title"
         append-outer-icon="check"
         @click:append-outer="updateTitle")
-    template(v-else-if="editRecipients || !isRecipients")
-      UserSelector(:previous="conversation.members" :contacts="contacts" @done="updateRecipients($event)")
     template(v-else-if="menu === 'color'")
       v-select.color(name="color" label="Set Message Color" single-line full-width hide-details return-object
         v-model="color"
@@ -49,11 +49,11 @@
             v-list-tile-action
               v-icon notifications_off
             v-list-tile-title Mute Notifications
-          v-list-tile(v-if="isMulti" @click="editTitle = true")
+          v-list-tile(v-if="isMulti" @click="menu = 'title'")
             v-list-tile-action
-              v-icon edit
+              v-icon title
             v-list-tile-title Rename Conversation
-          v-list-tile(v-if="isMulti && isOwner" @click="editRecipients = true")
+          v-list-tile(v-if="isMulti && isOwner" @click="menu = 'recipients'")
             v-list-tile-action
               v-icon person_add
             v-list-tile-title Edit Recipients
@@ -65,7 +65,7 @@
             v-list-tile-action
               v-icon color_lens
             v-list-tile-title Set Message Color
-          v-list-tile(@click="")
+          v-list-tile(@click="menu = 'info'")
             v-list-tile-action
               v-icon info
             v-list-tile-title Info
@@ -82,14 +82,13 @@ import UserSelector from "@/components/chat/UserSelector.vue";
       menu: "",
       title: "",
       query: "",
+      mute: "",
       color: 0,
       colors: [
         {title: "Red", hex: "#FF0000"},
         {title: "Green", hex: "#00FF00"},
         {title: "Blue", hex: "#0000FF"}
-      ],
-      editTitle: false,
-      editRecipients: false
+      ]
     };
   },
   created: function() {
@@ -103,7 +102,7 @@ import UserSelector from "@/components/chat/UserSelector.vue";
       return this.conversation.members.length > 2;
     },
     isOwner: function() {
-      return this.conversation.created === this.$store.getters.user.username;
+      return this.conversation.creator === this.$store.getters.user.username;
     },
     isRecipients: function() {
       return this.conversation.members.length > 1;
@@ -111,12 +110,12 @@ import UserSelector from "@/components/chat/UserSelector.vue";
   },
   methods: {
     updateTitle: function() {
-      this.editTitle = false;
+      this.menu = "";
       this.$emit('updateTitle', this.title);
       this.title = "";
     },
     updateRecipients: function(recipients) {
-      this.editRecipients = false;
+      this.menu = "";
       this.$emit('updateRecipients', recipients);
     },
     getOtherMembers: function(members) {
