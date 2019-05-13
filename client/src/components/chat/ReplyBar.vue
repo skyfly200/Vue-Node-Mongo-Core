0c57321ce0c7f7dace86dbb022258733657a6415
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-13 00:47:15 -0600

    add picker and toggle icon btn to ReplyBar, textarea size auto grow

--- client/src/components/chat/ReplyBar.vue
+++ client/src/components/chat/ReplyBar.vue
@@ -1,18 +1,42 @@
 <template lang="pug">
-  v-form.reply-bar(@submit.prevent="send")
-    v-text-field(name="reply" v-model="reply" label="Reply" :disabled="disabled" autofocus single-line full-width hide-details)
-    v-btn(fab small color="green" :disabled="disabled" @click="send")
-      v-icon send
+  .reply
+    Picker(v-show="showEmojiPicker" title="Pick your emoji..." emoji="point_up" set="messenger" @select="addEmoji")
+    v-form.reply-bar(@submit.prevent="send")
+      v-textarea(autofocus auto-grow no-resize single-line full-width hide-details solo
+        name="reply" ref="textarea" rows=1
+        append-outer-icon="insert_emoticon"
+        @click:append-outer.prevent="toggleShowEmojiPicker"
+        v-model="reply" label="Reply" :disabled="disabled")
+      v-btn(fab small color="green" :disabled="disabled" @click="send")
+        v-icon send
 </template>
 <script>
 import { Component, Vue } from "vue-property-decorator";
+import { Picker } from 'emoji-mart-vue';
 
 @Component({
+  components: {Picker},
   props: ["disabled"],
   data: () => ({
-    reply: ""
+    reply: "",
+    showEmojiPicker: false
   }),
   methods: {
+    addEmoji: function(emoji) {
+      const textarea = this.$refs.textarea
+      const cursorPosition = textarea.selectionEnd
+      const start = this.reply.substring(0, textarea.selectionStart)
+      const end = this.reply.substring(textarea.selectionStart)
+      console.log(textarea, textarea.selectionEnd);
+      this.reply += emoji.native;
+      textarea.focus()
+      this.$nextTick(() => {
+        textarea.selectionEnd = cursorPosition + emoji.native.length
+      })
+    },
+    toggleShowEmojiPicker: function() {
+      this.showEmojiPicker = !this.showEmojiPicker;
+    },
     send: function() {
       if (this.reply) {
         this.$emit('send', this.reply);
