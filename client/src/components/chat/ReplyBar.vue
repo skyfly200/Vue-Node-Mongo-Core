55359b2a07710ab408077efd92a556f99fd38e23
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-13 01:51:20 -0600

    setup link parsing function and preview API

--- client/src/components/chat/ReplyBar.vue
+++ client/src/components/chat/ReplyBar.vue
@@ -2,9 +2,10 @@
   .reply
     Picker(v-show="showEmojiPicker" title="Pick your emoji..." emoji="point_up" set="messenger" @select="addEmoji")
     v-form.reply-bar(@submit.prevent="send")
-      v-textarea(autofocus auto-grow no-resize single-line full-width hide-details solo
+      v-textarea(autofocus auto-grow single-line full-width hide-details solo
         name="reply" ref="textarea" rows=1
         append-outer-icon="insert_emoticon"
+        @input="parseReply"
         @click:append-outer.prevent="toggleShowEmojiPicker"
         v-model="reply" label="Reply" :disabled="disabled")
       v-btn(fab small color="green" :disabled="disabled" @click="send")
@@ -22,6 +23,39 @@ import { Picker } from 'emoji-mart-vue';
     showEmojiPicker: false
   }),
   methods: {
+    debounce: function(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this;
+        var args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    },
+    parseReply: function() {
+      console.log("ran")
+      var url = this.urlify(this.reply);
+      var getPreview = this.debounce(this.getLinkPreview(url[0]), 3000);
+      if (url && url[0]) getPreview();
+    },
+    getLinkPreview: function(url) {
+      this.$http.post('https://api.linkpreview.net', {
+          q: url,
+          key: '5cd91310830534140fe6a9358de3c9d660225c4c2fcb4'
+        }).then(resp => {
+          console.log(resp.data)
+        }).catch(error => {});
+    },
+    urlify: function(text) {
+      var urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+      return text.match(urlRegex);
+    },
     addEmoji: function(emoji) {
       const textarea = this.$refs.textarea
       const cursorPosition = textarea.selectionEnd
