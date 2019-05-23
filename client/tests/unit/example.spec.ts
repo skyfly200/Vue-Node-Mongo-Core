29fe01d1c4ac31197a0ad1ffb165a2108c1a7292
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-23 02:12:36 -0600

    add more tests to cover FabBtn component

--- client/tests/unit/example.spec.ts
+++ client/tests/unit/example.spec.ts
@@ -1,34 +1,51 @@
-//import { expect } from "chai";
-var expect = require('expect')
-import { shallowMount } from "@vue/test-utils";
+var expect = require('expect');
+import { mount, createLocalVue } from '@vue/test-utils';
+import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
 import FabBtn from "@/components/FabBtn.vue";
 
 describe("FabBtn.vue", () => {
+  const routes = [
+    { path: '/items/:item_id/edit', name: 'item-edit' }
+  ];
+
+  const router = new VueRouter({ routes });
+
   const icon = "check";
   const tooltip = {text: "new message", bottom: true};
   const color = "red";
   const size = "small";
-  const wrapper = shallowMount(FabBtn, {
+
+  const localVue = createLocalVue();
+  localVue.use(VueRouter);
+  localVue.use(Vuetify);
+
+  const wrapper = mount(FabBtn, {
+    localVue: localVue,
+    router,
     propsData: { icon, tooltip, color, size }
   });
-  console.log(wrapper.html())
+
+  beforeEach(() => {});
+
+  console.log(wrapper.html());
 
   it("renders tooltip text", () => {
     expect(wrapper.text()).toContain(tooltip.text);
   });
 
-  // it('renders the correct markup', () => {
-  //   expect(wrapper.html()).contain('<span class="count">0</span>')
-  // })
+  //it's also easy to check for the existence of elements
+  it('has a button', () => {
+    expect(wrapper.contains('button')).toBe(true);
+  })
 
-  // it's also easy to check for the existence of elements
-  // it('has a button', () => {
-  //   expect(wrapper.contains('v-button')).to.equal(true)
-  // })
+  it('renders the icon', () => {
+    expect(wrapper.html()).toContain(icon + '</i>');
+  })
 
-  // it('button click should emit select', () => {
-  //   const button = wrapper.find('v-btn')
-  //   button.trigger('click')
-  //   // assert select is emited
-  // })
+  it('button click should emit select', () => {
+    const button = wrapper.find('button');
+    button.trigger('click');
+    expect(wrapper.emitted().select).toBeTruthy();
+  })
 });
