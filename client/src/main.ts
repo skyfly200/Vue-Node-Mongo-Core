0b27d73e37ff02afc721bf7ba1e4378959a1c902
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-23 18:32:12 -0600

    add capitalize filter globaly

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,16 +1,17 @@
 import Vue from "vue";
-import './plugins/vuetify'
+import "./plugins/vuetify";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 import Axios from "axios";
-import SocketIO from 'socket.io-client';
-import VueSocketIO from 'vue-socket.io';
+import SocketIO from "socket.io-client";
+import VueSocketIO from "vue-socket.io";
 import App from "./App.vue";
 
-Vue.use(new VueSocketIO({
+Vue.use(
+  new VueSocketIO({
     debug: true,
-    connection: SocketIO('http://localhost:1234'),
+    connection: SocketIO("http://localhost:1234"),
     vuex: {
       store,
       actionPrefix: "socket_",
@@ -19,11 +20,16 @@ Vue.use(new VueSocketIO({
   })
 );
 
+Vue.filter("capitalize", function(value: string) {
+  if (!value) return "";
+  return value.charAt(0).toUpperCase() + value.slice(1);
+});
+
 Vue.prototype.$http = Axios;
 
-const token = localStorage.getItem('token');
+const token = localStorage.getItem("token");
 if (token) {
-  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+  Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
 }
 
 Vue.config.productionTip = false;
